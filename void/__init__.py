642b63f5825084848f57df2c693b361c25e7852c
Andrew Leacock <andrewcleacock@gmail.com>
2023-05-31 20:39:57 -0400

    moved files to correct directory

--- void/__init__.py
+++
@@ -1,1 +0,0 @@
-from .void import VOID
--- void/void-cli.py
+++ void/void-cli.py
@@ -2,8 +2,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='stat converter for jiu jistics')
 
-parser.add_argument('-iD', metavar='import directory', dest='import', type='str', help='The directory where your excel import files will be located')
+parser.add_argument('-iD', metavar='import directory', dest='import', type=str, help='The directory where your excel import files will be located')
 
-args = parser.parse_args()
+args = parser.parse_args
 
-void = args
+print(args)
