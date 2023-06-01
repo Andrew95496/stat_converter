9ceec408e2cc3493670b25e6c2acd1d2684360fb
Andrew Leacock <andrewcleacock@gmail.com>
2023-05-31 22:27:17 -0400

    Work on `void-cli.py`

--- void/void-cli.py
+++ void/void-cli.py
@@ -2,6 +2,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='stat converter for jiu jistics')
 
-parser.add_argument('-iD', metavar='import directory', dest='import', type=str, help='The directory where your excel import files will be located')
+parser.add_argument('-imp', metavar='import directory', dest='import', type=str, help='The directory where your excel import files will be located')
 
 args = parser.parse_args
+
+imports = args.imp
