9a4fb128a76c433f7c750094fbc1ca182ea58d5a
Andrew Leacock <andrewcleacock@gmail.com>
2023-05-31 21:44:36 -0400

    Add argparse module(#7)

--- void/void-cli.py
+++ void/void-cli.py
@@ -5,5 +5,3 @@ parser = argparse.ArgumentParser(description='stat converter for jiu jistics')
 parser.add_argument('-iD', metavar='import directory', dest='import', type=str, help='The directory where your excel import files will be located')
 
 args = parser.parse_args
-
-print(args)
