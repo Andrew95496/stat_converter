fa5042c1b5a67792b70b1e1ddd9bec355024798b
Andrew Leacock <andrewcleacock@gmail.com>
2023-06-04 17:02:20 -0400

    Add config params

--- void/configs/configs.py
+++ void/configs/configs.py
@@ -7,13 +7,15 @@ class Config():
     config = configparser.ConfigParser()
     config.read(f'{os.getcwd()}/void/configs/configs.cfg')
     IMPORT_DIR = config['DEFAULTS']['IMPORT_DIR']
+    EXPORT_DIR = config['DEFAULTS']['EXPORT_DIR']
     ATHLETES = json.loads(config.get("DEFAULTS", "ATHLETES"))
     SHEET_NAME = config['DEFAULTS']['SHEET_NAME']
 
 
-    def __init__(self, ROOT_DIR: str =f"{os.getcwd()}/athletes/", IMPORT_DIR: str = IMPORT_DIR, ATHLETES: list[str] = ATHLETES, SHEET_NAME: str = SHEET_NAME) -> None:
+    def __init__(self, ROOT_DIR: str =f"{os.getcwd()}/athletes/", IMPORT_DIR: str = IMPORT_DIR, EXPORT_DIR: str = EXPORT_DIR, ATHLETES: list[str] = ATHLETES, SHEET_NAME: str = SHEET_NAME) -> None:
         self.ROOT_DIR = ROOT_DIR
         self.IMPORT_DIR = IMPORT_DIR
+        self.EXPORT_DIR = EXPORT_DIR
         self.ATHLETES = ATHLETES
         self.SHEET_NAME = SHEET_NAME
 
